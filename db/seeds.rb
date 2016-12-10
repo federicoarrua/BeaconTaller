85e471734939b3d352a1c445152e9f5d765be2a6
Federico <federico.arrua.26@outlook.com>
2016-12-10 16:29:33 -0300

    Fixed bug in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 MajorRegion.create(major_id:4660,description:"Esta región es usada para referenciar a todos los beacons del primer piso")
 MinorRegion.create(minor_id:64001,description:"Esta región es usada para referenciar a todos los beacons del primer aula de cada piso")
-Beacon.create(major_id:1,minor_id:1,description:"Encontraste el beacon con major y minor 1,1 Felicidades!!")
+Beacon.create(major_id:4660,minor_id:64001,description:"Encontraste el beacon con major y minor 1,1 Felicidades!!")
 
 Device.create(device_id:"example@mail.com")
 User.create(email:"example@mail.com",password:"password",password_confirmation:"password")
